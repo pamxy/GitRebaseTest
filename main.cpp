26acfe346809a90a94e5d00c24d6317c45fb20b8
liangguipeng <liangguipeng@jz.com>
2018-10-15 05:02:02 -0400

    1.2dfdsf;

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, Pamxy!" << std::endl;
     std::cout << "Hello, 1!" << std::endl;
+    std::cout << "Hello, 2!" << std::endl;
     return 0;
 }
